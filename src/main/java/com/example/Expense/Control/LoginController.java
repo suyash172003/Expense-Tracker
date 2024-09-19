56e481cf563193a8ecba32459705d992e5772258
rocksuyash <suyash172003@gmail.com>
2024-09-19 20:32:13 +0530

    Made changes for CSS, Login and Signup Authentication

--- src/main/java/com/example/Expense/Control/LoginController.java
+++ src/main/java/com/example/Expense/Control/LoginController.java
@@ -7,13 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.Expense.Model.Login;
+import com.example.Expense.Model.Register;
 import com.example.Expense.Repository.LoginRepository;
+import com.example.Expense.Repository.RegisterRepository;
 
 @Controller
 public class LoginController {
 	
 	@Autowired
-	LoginRepository loginRepo;
+	private RegisterRepository registering;
 	
 	@GetMapping("/login")
 	public String loginGet(Model model) {
@@ -23,7 +25,12 @@ public class LoginController {
 	
 	@PostMapping("/login")
 	public String loginPut(@ModelAttribute Login login) {
-		loginRepo.save(login);
-		return "redirect:/about";
+		Register val=registering.findByEmail(login.getEmail());
+		if(val!=null&&val.getEmail().compareTo(login.getEmail())==0&&val.getPassword().compareTo(login.getPassword())==0) {
+			return "redirect:/expense";
+		}
+		else {
+			return "redirect:/login";
+		}
 	}
 }
--- src/main/java/com/example/Expense/Repository/RegisterRepository.java
+++ src/main/java/com/example/Expense/Repository/RegisterRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.Expense.Model.Register;
 
 public interface RegisterRepository extends JpaRepository<Register,Long>{
-
+		Register findByEmail(String email);
 }
